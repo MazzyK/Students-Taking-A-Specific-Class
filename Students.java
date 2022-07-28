b29b55b06bab1d479ab614cc8b697eee82a2e0a7
Maz <mkoutsikou@gmail.com>
2022-07-28 14:55:32 +0300

    Create Students.java

---
+++ Students.java
@@ -0,0 +1,18 @@
+public class Students {
+
+    private String name;
+    private String takesclass;
+
+    public Students(String name, String takesclass) {
+        this.name = name;
+        this.takesclass = takesclass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTakesclass() {
+        return takesclass;
+    }
+}
